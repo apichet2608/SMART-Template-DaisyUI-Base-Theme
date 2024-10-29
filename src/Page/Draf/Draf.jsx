71c0401cbb67de79757a77134b48be6dedb09c9b
Apichet Juntodum <Apichet.jtd@outlook.com>
2024-10-29 16:38:27 +0700

    dev size

--- src/Page/Draf/Draf.jsx
+++ src/Page/Draf/Draf.jsx
@@ -11,7 +11,7 @@ function Draf() {
         webkitallowfullscreen="true"
         allow="autoplay; fullscreen; xr-spatial-tracking"
         src="https://sketchfab.com/models/46e91129343a4aaf80fbd665c5b44c14/embed"
-        className="w-full h-screen"
+        className=" w-96 h-96"
       ></iframe>
     </>
   );
