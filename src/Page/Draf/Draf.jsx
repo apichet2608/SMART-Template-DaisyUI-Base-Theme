cf768fe7b9112ca6a7b19542c6b6c5824f32ee31
Apichet Juntodum <Apichet.jtd@outlook.com>
2024-10-29 16:45:05 +0700

    dev

--- src/Page/Draf/Draf.jsx
+++ src/Page/Draf/Draf.jsx
@@ -1,20 +1,7 @@
 import React from "react";
 
 function Draf() {
-  return (
-    <>
-      <iframe
-        title="Jungle Animal: Cartoon Hippo"
-        frameBorder="0"
-        allowFullScreen
-        mozallowfullscreen="true"
-        webkitallowfullscreen="true"
-        allow="autoplay; fullscreen; xr-spatial-tracking"
-        src="https://sketchfab.com/models/46e91129343a4aaf80fbd665c5b44c14/embed"
-        className=" w-96 h-96"
-      ></iframe>
-    </>
-  );
+  return <div>Draf</div>;
 }
 
 export default Draf;
